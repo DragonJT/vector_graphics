ad64f983099878907181e54d4637f70bd12ab7b2
DragonJT <jonathanthoms006@gmail.com>
2024-04-07 21:59:31 +0100

    added portals and enemies

--- src/vector_graphics.rs
+++ src/vector_graphics.rs
@@ -7,9 +7,23 @@ struct Drag{
     y2:f32,
 }
 
+enum CollisionType{
+    Bounce,
+    PortalTo(usize),
+    None,
+}
+
 enum Mode{
     Play,
     Edit,
+}
+
+enum ObjectType{
+    Player,
+    Enemy,
+    Block,
+    PortalIn,
+    PortalOut,
 }
 
 struct Rect{
@@ -28,6 +42,11 @@ impl Rect{
         [self.x+self.width/2.0, self.y+self.height/2.0]
     }
 
+    fn set_center(&mut self, center:[f32;2]){
+        self.x = center[0] - self.width/2.0;
+        self.y = center[1] - self.height/2.0;
+    }
+
     fn overlaps(a:&Rect, b:&Rect) -> bool{
         let center_a = a.center();
         let center_b = b.center();
@@ -39,30 +58,38 @@ impl Rect{
     }
 }
 
-struct Player{
-    active:bool,
+struct Object{
+    object_type:ObjectType,
     rect:Rect,
     velocity_x:f32,
     velocity_y:f32,
-    grounded:bool,
+    gravity:f32,
+    ai_direction:f32,
+    collision_type:CollisionType,
 }
 
-
 pub struct VectorGraphics {
-    blocks:Vec<Rect>,
-    player:Player,
+    objects:Vec<Object>,
     drag:Drag,
     mouse_position:[f32;2],
     mode:Mode,
-    gravity:f32,
-    player_speed:f32,
+    speed:f32,
     jump_force:f32,
     left_arrow:bool,
     right_arrow:bool,
     up_arrow:bool,
+    last_portal_in:usize,
 }
 
 impl VectorGraphics {
+
+    fn is_playing(&self) -> bool{
+        match self.mode {
+            Mode::Play => true,
+            _ => false,
+        }
+    }
+
     fn abs_rect(x:f32, y:f32, w:f32, h:f32) -> (f32, f32, f32, f32){
         let mut result_x = x;
         let mut result_y = y;
@@ -79,9 +106,9 @@ impl VectorGraphics {
         (result_x, result_y, result_w, result_h)
     }
 
-    fn find_block_at_point(&self, point:[f32;2]) -> Option<usize>{
-        for i in 0..self.blocks.len(){
-            if self.blocks[i].contains(point){
+    fn find_object_at_point(&self, point:[f32;2]) -> Option<usize>{
+        for i in 0..self.objects.len(){
+            if self.objects[i].rect.contains(point){
                 return Some(i);
             }
         }
@@ -90,23 +117,16 @@ impl VectorGraphics {
 
     pub fn new() -> VectorGraphics{
         return VectorGraphics { 
-            blocks:Vec::new(), 
-            player:Player { 
-                active: false, 
-                rect: Rect { x: 0.0, y: 0.0, width: 0.0, height: 0.0 }, 
-                velocity_x: 0.0, 
-                velocity_y: 0.0, 
-                grounded: true 
-            },
+            objects:Vec::new(), 
             drag:Drag { dragging: false, x: 0.0, y: 0.0, x2: 0.0, y2: 0.0 }, 
             mouse_position:[0.0,0.0],
             mode:Mode::Edit,
-            gravity:0.3,
-            player_speed:5.0,
+            speed:5.0,
             jump_force: 14.0,
             left_arrow:false,
             right_arrow:false,
             up_arrow:false,
+            last_portal_in:0,
          };
     }
 
@@ -137,8 +157,8 @@ impl VectorGraphics {
             Mode::Edit => {
                 match key{
                     winit::keyboard::KeyCode::Backspace=>{
-                        match self.find_block_at_point(self.mouse_position){
-                            Some(id) => {self.blocks.remove(id);}
+                        match self.find_object_at_point(self.mouse_position){
+                            Some(id) => {self.objects.remove(id);}
                             _ => {}
                         }
                     }
@@ -152,13 +172,40 @@ impl VectorGraphics {
                         }
                     }
                     winit::keyboard::KeyCode::KeyP=>{
-                        match self.find_block_at_point(self.mouse_position){
+                        match self.find_object_at_point(self.mouse_position){
                             Some(id) => {
-                                self.player.rect = self.blocks.remove(id);
-                                self.player.velocity_x = 0.0;
-                                self.player.velocity_y = 0.0;
-                                self.player.grounded = false;
-                                self.player.active = true;
+                                self.objects[id].object_type = ObjectType::Player;
+                                self.objects[id].gravity = 0.3;
+                            }
+                            _ => {}
+                        }
+                    }
+                    winit::keyboard::KeyCode::KeyE=>{
+                        match self.find_object_at_point(self.mouse_position){
+                            Some(id) => {
+                                self.objects[id].object_type = ObjectType::Enemy;
+                                self.objects[id].gravity = 0.3;
+                                self.objects[id].ai_direction = -1.0;
+                            }
+                            _ => {}
+                        }
+                    }
+                    winit::keyboard::KeyCode::KeyI=>{
+                        match self.find_object_at_point(self.mouse_position){
+                            Some(id) => {
+                                self.objects[id].object_type = ObjectType::PortalIn;
+                                self.objects[id].collision_type = CollisionType::None;
+                                self.last_portal_in = id;
+                            }
+                            _ => {}
+                        }
+                    }
+                    winit::keyboard::KeyCode::KeyO => {
+                        match self.find_object_at_point(self.mouse_position){
+                            Some(id) => {
+                                self.objects[id].object_type = ObjectType::PortalOut;
+                                self.objects[id].collision_type = CollisionType::None;
+                                self.objects[self.last_portal_in].collision_type = CollisionType::PortalTo(id);
                             }
                             _ => {}
                         }
@@ -181,11 +228,20 @@ impl VectorGraphics {
                 if self.drag.dragging{
                     self.drag.dragging = false;
                     let abs_rect = Self::abs_rect(self.drag.x, self.drag.y, self.drag.x2 - self.drag.x, self.drag.y2 - self.drag.y);
-                    self.blocks.push(Rect {
+                    let rect = Rect {
                         x: abs_rect.0, 
                         y: abs_rect.1, 
                         width: abs_rect.2, 
                         height: abs_rect.3,
+                    };
+                    self.objects.push(Object { 
+                        object_type: ObjectType::Block, 
+                        rect: rect, 
+                        gravity: 0.0,
+                        velocity_x: 0.0, 
+                        velocity_y: 0.0, 
+                        ai_direction: 0.0,
+                        collision_type: CollisionType::Bounce,
                     });
                 }
             }
@@ -193,32 +249,59 @@ impl VectorGraphics {
         }
     }
 
-    fn overlaps_any_block(&self, rect:&Rect)->bool{
-        for o in &self.blocks{
-            if Rect::overlaps(rect, o){
-                return true;
+    fn overlaps_any_block(&self, object_id:usize)->Option<usize>{
+        for i in 0..self.objects.len(){
+            if i!=object_id && Rect::overlaps(&self.objects[object_id].rect, &self.objects[i].rect){
+                return Some(i);
             }
         }
-        return false;
+        return None;
     }
 
-    fn slide_x(&mut self, distance:f32){
-        self.player.rect.x += distance;
-        if self.overlaps_any_block(&self.player.rect) { 
-            self.player.rect.x -= distance;
-            self.player.velocity_x = 0.0;
+    fn slide_x(&mut self, id:usize, distance:f32) -> bool{
+        self.objects[id].rect.x += distance;
+        match self.overlaps_any_block(id) {
+            Some(other_id) => {
+                match self.objects[other_id].collision_type {
+                    CollisionType::Bounce => {
+                        self.objects[id].rect.x -= distance;
+                        self.objects[id].velocity_x = 0.0;
+                        return true;
+                    }
+                    CollisionType::PortalTo(location_id) => {
+                        let location = self.objects[location_id].rect.center();
+                        self.objects[id].rect.set_center(location);
+                        return false;
+                    }
+                    _ => return false,
+                }
+            } 
+            None => {}
         }
+        false
     }
 
-    fn slide_y(&mut self, distance:f32){
-        self.player.rect.y += distance;
-        if self.overlaps_any_block(&self.player.rect){
-            self.player.rect.y -= distance;
-            if distance>=0.0 {
-                self.player.grounded = true;
+    fn slide_y(&mut self, id:usize, distance:f32) -> bool{
+        self.objects[id].rect.y += distance;
+        match self.overlaps_any_block(id) {
+            Some(other_id) => {
+                match self.objects[other_id].collision_type {
+                    CollisionType::Bounce => {
+                        self.objects[id].rect.y -= distance;
+                        self.objects[id].velocity_y = 0.0;
+                        return true;
+                    }
+                    CollisionType::PortalTo(location_id) => {
+                        let location = self.objects[location_id].rect.center();
+                        self.objects[id].rect.set_center(location);
+                        return false;
+                    }
+                    _ => return false,
+                }
             }
-           self.player.velocity_y = 0.0;
+            None => {}
         }
+        false
     }
 
     pub fn update(&mut self, mesh:&mut crate::mesh::Mesh, queue:&wgpu::Queue){
@@ -226,31 +309,45 @@ impl VectorGraphics {
             let abs_rect = Self::abs_rect(self.drag.x, self.drag.y, self.drag.x2 - self.drag.x, self.drag.y2 - self.drag.y);
             mesh.add_rect(abs_rect.0, abs_rect.1, abs_rect.2, abs_rect.3, 0.0, 0.0, 1.0);
         }
-        match self.mode{
-            Mode::Play => {
-                if self.player.active{
-                    self.player.velocity_y += self.gravity;
-                    self.slide_x(self.player.velocity_x);
-                    self.slide_y(self.player.velocity_y);
-                    if self.left_arrow{
-                        self.slide_x(-self.player_speed);
+        if self.is_playing(){
+            for i in 0..self.objects.len(){
+                self.objects[i].velocity_y += self.objects[i].gravity;
+                self.slide_x(i, self.objects[i].velocity_x);
+                let vy = self.objects[i].velocity_y;
+                let grounded = self.slide_y(i, vy) && vy >= 0.0;
+                match self.objects[i].object_type {
+                    ObjectType::Player => {
+                        if self.left_arrow{
+                            self.slide_x(i, -self.speed);
+                        }
+                        if self.right_arrow{
+                            self.slide_x(i, self.speed);
+                        }
+                        if self.up_arrow && grounded{
+                            self.objects[i].velocity_y -= self.jump_force;
+                        }
                     }
-                    if self.right_arrow{
-                        self.slide_x(self.player_speed);
+                    ObjectType::Enemy => {
+                        if self.slide_x(i, self.speed * self.objects[i].ai_direction) {
+                            self.objects[i].ai_direction *= -1.0;
+                        }
+                        if grounded {
+                            self.objects[i].velocity_y -= self.jump_force;
+                        }
                     }
-                    if self.up_arrow && self.player.grounded{
-                        self.player.velocity_y -= self.jump_force;
-                    }
-                    self.player.grounded = false;
+                    _ => {}
                 }
             }
-            Mode::Edit => {}
         }
-        for block in &self.blocks{
-            mesh.add_rect(block.x, block.y, block.width, block.height, 0.025, 0.025, 0.025);
-        }
-        if self.player.active{
-            mesh.add_rect(self.player.rect.x, self.player.rect.y, self.player.rect.width, self.player.rect.height, 1.0, 0.5, 0.0);
+        for object in &self.objects{
+            let color = match object.object_type {
+                ObjectType::Block => (0.025,0.025,0.025),
+                ObjectType::Player => (1.0, 0.5, 0.0),
+                ObjectType::Enemy => (1.0, 0.0, 0.0),
+                ObjectType::PortalIn => (0.2, 1.0, 0.2),
+                ObjectType::PortalOut => (0.2, 0.2, 1.0),
+            };
+            mesh.add_rect(object.rect.x, object.rect.y, object.rect.width, object.rect.height, color.0, color.1, color.2);
         }
         mesh.update_queue(queue);
     }
